ac41451f4a02bf776c3b271127d370fa1e896888
mg-diego <diego.mg.umh@gmail.com>
2025-09-17 16:48:43 +0200

    Include near endpoint at API

--- application/api/server.py
+++ application/api/server.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from application.api.api import get_metro_router, get_bus_router, get_tram_router, get_rodalies_router, get_bicing_router, get_fgc_router
+from application.api.api import get_metro_router, get_bus_router, get_tram_router, get_rodalies_router, get_bicing_router, get_fgc_router, get_near_router
 
 def create_app(
     metro_service,
@@ -18,5 +18,7 @@ def create_app(
     app.include_router(get_fgc_router(fgc_service), prefix="/api/fgc")
     app.include_router(get_rodalies_router(rodalies_service), prefix="/api/rodalies")
     app.include_router(get_bicing_router(bicing_service), prefix="/api/bicing")
+
+    app.include_router(get_near_router(metro_service, bus_service, tram_service, rodalies_service, bicing_service, fgc_service), prefix="/api/near")
 
     return app
